7d12aab26406e2ceada7354a127840a9e3e177f8
Bernardo Giehl <56963492+bernardo-giehl@users.noreply.github.com>
2025-05-15 21:20:54 -0300

    fix: ajdust preview

---
+++ packages/docs/.storybook/preview.js
@@ -0,0 +1,13 @@
+import { themes } from '@storybook/theming'
+
+export const parameters = {
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    theme: themes.dark,
+  }
+}
